a8cfeecba1376b6e9081abd675c70914fa5ad472
papparazzo <pappi-@gmx.de>
2016-06-25 22:42:40 +0200

    some minor improvements

--- src/main.cpp
+++ src/main.cpp
@@ -31,11 +31,16 @@
 #include <iostream>
 #include <fstream>
 
+#include <thread>
+#include <mutex>
+
 #include <moba/log.h>
 #include <moba/msghandler.h>
 #include <moba/helper.h>
 
 #include <moba/version.h>
+#include <moba/message.h>
+#include <moba/jsonabstractitem.h>
 
 namespace {
     AppData appData = {
@@ -55,9 +60,9 @@ namespace {
         {Message::MT_ECHO_REQ,                "ECHO_REQ"               },
 
         {Message::MT_RESET_CLIENT,            "RESET_CLIENT"           },
-
         {Message::MT_SERVER_INFO_REQ,         "SERVER_INFO_REQ"        },
         {Message::MT_CON_CLIENTS_REQ,         "CON_CLIENTS_REQ"        },
+        {Message::MT_SELF_TESTING_CLIENT,     "SELF_TESTING_CLIENT"    },
 
         {Message::MT_GET_GLOBAL_TIMER,        "GET_GLOBAL_TIMER"       },
         {Message::MT_SET_GLOBAL_TIMER,        "SET_GLOBAL_TIMER"       },
@@ -77,13 +82,11 @@ namespace {
     };
 }
 
-bool prettyPrint(JsonItemPtr ptr) {
-    std::ofstream myfile;
-    myfile.open("out.txt", std::ios::app);
+void prettyPrint(JsonItemPtr ptr, std::ofstream &out) {
 
     if(!ptr) {
-        myfile << "{}";
-        return false;
+        out << "{}";
+        return;
     }
 
     std::string str = ptr->getJsonString();
@@ -93,24 +96,24 @@ bool prettyPrint(JsonItemPtr ptr) {
         switch(str[i]) {
             case '{':
             case '[':
-                myfile << str[i] << std::endl << std::setw(++indent * 4);
+                out << str[i] << std::endl << std::setw(++indent * 4);
                 break;
 
             case ',':
-                myfile << str[i] << std::endl << std::setw(indent * 4);
+                out << str[i] << std::endl << std::setw(indent * 4);
                 break;
 
             case '}':
             case ']':
-                myfile << std::endl << std::setw(--indent * 4) << str[i];
+                out << std::endl << std::setw(--indent * 4) << str[i];
                 break;
 
             default:
-                myfile << str[i];
+                out << str[i];
         }
     }
-    myfile << std::endl;
-    return true;
+    out << std::endl;
+    return;
 }
 
 void printCommands() {
@@ -126,6 +129,54 @@ void printCommands() {
     std::cout << "98: Puffer leeren" << std::endl;
     std::cout << "99: Programmende" << std::endl;
     std::cout << "?";
+}
+
+void printTimeStamp(std::ofstream &out) {
+    char buffer[25] = "";
+    struct timeb sTimeB;
+
+    ftime(&sTimeB);
+
+    strftime(buffer, 21, "%d.%m.%Y %H:%M:%S.", localtime(&sTimeB.time));
+    out <<
+        buffer << std::setw(4) << std::setfill('0') <<
+        sTimeB.millitm << std::setfill(' ');
+}
+
+template<typename T>
+void getData(const std::string &s, T &data) {
+    std::cout << s << "?";
+    std::cin >> data;
+    std::cout << std::endl;
+}
+
+std::string getString(const std::string &s) {
+    std::string str;
+    std::cout << s << "?";
+    char input[256];
+    std::cin.getline(input, sizeof(input));
+    return input;
+}
+
+bool getBool(const std::string &s) {
+    std::string str;
+    getData(s + "[t|f]", str);
+    if(str == "t") {
+        return true;
+    }
+    return false;
+}
+
+JsonThreeState::ThreeState getThreeState(const std::string &s) {
+    std::string str;
+    getData(s + "[on|off|auto]", str);
+    if(str == "on") {
+        return JsonThreeState::ON;
+    }
+    if(str == "off") {
+        return JsonThreeState::OFF;
+    }
+    return JsonThreeState::AUTO;
 }
 
 int main(int argc, char* argv[]) {
@@ -147,142 +198,208 @@ int main(int argc, char* argv[]) {
 
     MsgHandler msgHandler;
     MessagePtr msg;
-    try {
-      msgHandler.connect(appData.host, appData.port);
-      JsonArrayPtr groups(new JsonArray());
-      groups->push_back(toJsonStringPtr("BASE"));
-      groups->push_back(toJsonStringPtr("ENV"));
-      groups->push_back(toJsonStringPtr("SERV"));
-      groups->push_back(toJsonStringPtr("SYSTEM"));
+    std::ofstream myfile;
+    myfile.open("response.log", std::ios::app);
 
-      msgHandler.registerApp(
-          appData.appName,
-          Version(appData.version),
-          groups
-      );
+    try {
+        msgHandler.connect(appData.host, appData.port);
+        JsonArrayPtr groups(new JsonArray());
+        groups->push_back(toJsonStringPtr("BASE"));
+        groups->push_back(toJsonStringPtr("ENV"));
+        groups->push_back(toJsonStringPtr("SERV"));
+        groups->push_back(toJsonStringPtr("SYSTEM"));
+
+        msgHandler.registerApp(
+            appData.appName,
+            Version(appData.version),
+            groups
+        );
+        myfile << "######################################################" << std::endl;
+        myfile << "######             Application start            ######" << std::endl;
+        myfile << "######################################################" << std::endl;
     } catch(MsgHandlerException &e) {
         std::cerr << e.what() << std::endl;
         return (EXIT_FAILURE);
     }
+
+    bool running = true;
+
+    std::mutex myMutex;
+
+    std::thread t([&] {
+        while(running) {
+            myMutex.lock();
+            MessagePtr msg = msgHandler.recieveMsg();
+            if(msg) {
+                printTimeStamp(myfile);
+                myfile << " Response: " << msg->msgTypeAsString() << std::endl;
+                prettyPrint(msg->getData(), myfile);
+                myfile << "******************************************************" << std::endl;
+            }
+            myMutex.unlock();
+            usleep(250);
+        }
+    });
 
     while(1) {
         try {
             printCommands();
 
             int cmd = 0;
-            std::string str;
-
             std::cin >> cmd;
+
+            myMutex.lock();
+            printTimeStamp(myfile);
+            myfile <<
+                " Request: " <<
+                *Message::convertToString(static_cast<Message::MessageType>(cmd)) <<
+                std::endl;
+            myfile << "******************************************************" << std::endl;
 
             switch(cmd) {
                 case Message::MT_VOID:
                     msgHandler.sendVoid();
                     break;
 
-                case Message::MT_ECHO_REQ:
-                    std::cout << "Data?";
-                    std::cin >> str;
-                    std::cout << std::endl;
+                case Message::MT_ECHO_REQ: {
+                    std::string str;
+                    getData("Data", str);
                     msgHandler.sendEchoReq(str);
                     break;
+                }
 
-                case Message::MT_RESET_CLIENT:
-                    std::cout << "AppId?";
-                    std::cin >> cmd;
-                    std::cout << std::endl;
-                    msgHandler.sendResetClient(cmd);
+                case Message::MT_RESET_CLIENT: {
+                    int id = 0;
+                    getData("AppId", id);
+                    msgHandler.sendResetClient(id);
                     break;
+                }
 
-                case Message::MT_SERVER_INFO_REQ:
+                case Message::MT_SELF_TESTING_CLIENT: {
+                    int id = 0;
+                    getData("AppId", id);
+                    msgHandler.sendSelfTestingClient(id);
+                    break;
+                }
+
+                case Message::MT_SERVER_INFO_REQ: {
                     msgHandler.sendServerInfoReq();
                     break;
+                }
 
-                case Message::MT_CON_CLIENTS_REQ:
+                case Message::MT_CON_CLIENTS_REQ: {
                     msgHandler.sendConClientsReq();
                     break;
+                }
 
-                case Message::MT_GET_GLOBAL_TIMER:
+                case Message::MT_GET_GLOBAL_TIMER: {
                     msgHandler.sendGetGlobalTimer();
                     break;
+                }
 
-                case Message::MT_SET_GLOBAL_TIMER:
-                    //msgHandler.sendSetGlobalTimer();
+                case Message::MT_SET_GLOBAL_TIMER: {
+                    std::string date = "Sa 10:00";  // FIXME getString("DD hh:mm (e.g. Sa 10:00)");
+                    unsigned int intervall = 0;
+                    getData("Seconds (modulo 5)", intervall);
+                    unsigned int multiplicator = 0;
+                    getData("Multiplicator (realtime * multiplicator = modeltime)", multiplicator);
+                    msgHandler.sendSetGlobalTimer(date, intervall, multiplicator);
                     break;
+                }
 
                 case Message::MT_GET_ENVIRONMENT:
                     msgHandler.sendGetEnvironment();
                     break;
 
                 case Message::MT_SET_ENVIRONMENT:
-                    msgHandler.sendSetEnvironment();
+                    msgHandler.sendSetEnvironment(
+                        getThreeState("thunder"),
+                        getThreeState("wind"),
+                        getThreeState("rain"),
+                        getBool("curtainUp"),
+                        getBool("mainLightON"),
+                        getThreeState("aux01"),
+                        getThreeState("aux02"),
+                        getThreeState("aux03")
+                    );
                     break;
 
-                case Message::MT_SET_AUTO_MODE:
-                    std::cout << "t|f|n?";
-                    std::cin >> str;
-                    std::cout << std::endl;
-                    if(str == "t") {
-                        msgHandler.sendSetAutoMode(true);
-                    }
-                    if(str == "f") {
-                        msgHandler.sendSetAutoMode(false);
-                    }
-                    if(str == "n") {
-                        msgHandler.sendSetAutoMode();
-                    }
+                case Message::MT_SET_AUTO_MODE: {
+                    msgHandler.sendSetAutoMode(getBool(""));
                     break;
+                }
 
-                case Message::MT_GET_COLOR_THEME:
-                case Message::MT_SET_COLOR_THEME:
-                case Message::MT_EMERGENCY_STOP:
-                case Message::MT_EMERGENCY_STOP_CLEARING:
-                case Message::MT_GET_HARDWARE_STATE:
-                case Message::MT_SET_HARDWARE_STATE:
-                case Message::MT_HARDWARE_SHUTDOWN:
+                case Message::MT_GET_COLOR_THEME: {
+                    msgHandler.sendGetColorTheme();
+                    break;
+                }
+
+                case Message::MT_SET_COLOR_THEME: {
+                    msgHandler.sendSetColorTheme(
+                        getString("dimTime [hh:mm]"),
+                        getString("brightTime [hh:mm]")
+                    );
+                    break;
+                }
+
+                case Message::MT_EMERGENCY_STOP: {
+                    msgHandler.sendEmergencyStop();
+                    break;
+                }
+
+                case Message::MT_EMERGENCY_STOP_CLEARING: {
+                    msgHandler.sendEmergencyStopClearing();
+                    break;
+                }
+
+                case Message::MT_GET_HARDWARE_STATE: {
+                    msgHandler.sendGetHardwareState();
+                    break;
+                }
+
+                case Message::MT_SET_HARDWARE_STATE: {
+                    MsgHandler::HardwareState hs = MsgHandler::HS_ERROR;
+                    std::string s;
+                    getData("ready, error, standby, power off [r|e|s|p]", s);
+                    if(s == "r") {
+                        hs = MsgHandler::HS_READY;
+                    } else if(s == "s") {
+                        hs = MsgHandler::HS_STANDBY;
+                    } else if(s == "p") {
+                        hs = MsgHandler::HS_POWER_OFF;
+                    }
+                    msgHandler.sendSetHardwareState(hs);
+                    break;
+                }
+
+                case Message::MT_HARDWARE_SHUTDOWN: {
                     msgHandler.sendHardwareShutdown();
                     break;
+                }
 
-                case Message::MT_HARDWARE_RESET:
+                case Message::MT_HARDWARE_RESET: {
                     msgHandler.sendHardwareReset();
                     break;
+                }
 
-                case Message::MT_HARDWARE_SWITCH_STANDBY:
+                case Message::MT_HARDWARE_SWITCH_STANDBY: {
                     msgHandler.sendHardwareSwitchStandby();
                     break;
+                }
 
-                case 98:
+                case 98: {
                     break;
+                }
 
-                case 99:
+                case 99: {
                     msgHandler.sendClientClose();
+                    myMutex.unlock();
+                    running = false;
+                    t.join();
                     return EXIT_SUCCESS;
+                }
             }
-
-            while(msg = msgHandler.recieveMsg()) {
-                prettyPrint(msg->getData());
-            }
-
-
-
-
-
-
-
-
-/*
-            //msgHandler.sendServerSleep();
-            //msg = msgHandler.recieveMsg();
-            //prettyPrint(msg.getData()->getJsonString());
-
-            //msgHandler.sendHardwareStateRequest();
-            //msg = msgHandler.recieveMsg();
-            //prettyPrint(msg.getData()->getJsonString());
-
-            //msgHandler.sendServerShutdown();
-            //msg = msgHandler.recieveMsg();
-
-*/
-
+            myMutex.unlock();
         } catch(std::exception &e) {
             std::cerr << e.what() << std::endl;
             return (EXIT_FAILURE);
