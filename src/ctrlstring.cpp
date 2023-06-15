cdbfe93344a80a6c0935f85416f3b0bfb0f4468c
papparazzo <pappi-@gmx.de>
2023-06-15 22:09:37 +0200

    working on gtk4

--- src/ctrlstring.cpp
+++ src/ctrlstring.cpp
@@ -3,9 +3,9 @@
 #include <string>
 
 CtrlString::CtrlString() {
-    m_VBox.pack_start(m_HBox_String, Gtk::PACK_SHRINK);
-    m_HBox_String.pack_start(m_Label_String, Gtk::PACK_SHRINK);
-    m_HBox_String.pack_end(m_Entry_String, Gtk::PACK_SHRINK);
+    m_VBox.append(m_HBox_String);
+    m_HBox_String.append(m_Label_String);
+    m_HBox_String.append(m_Entry_String);
 }
 
 CtrlString::~CtrlString() {
@@ -14,8 +14,8 @@ CtrlString::~CtrlString() {
 void CtrlString::init(const std::string &caption, Gtk::ScrolledWindow &container) {
     m_Entry_String.set_text("");
     m_Label_String.set_label(caption);
-    container.add(m_VBox);
-    container.show_all_children();
+    container.set_child(m_VBox);
+  //  container.show_all_children();
 }
 
 long CtrlString::get_integer() const {
